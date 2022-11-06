2a08a3e43190df96fc255ffd9d2762634c6c3df9
Kobi S <kobi.shasha@gmail.com>
2022-11-06 15:36:53 +0200

    Baby steps

---
+++ src/Components/ex7/ClockClass/ClockClass.tsx
@@ -0,0 +1,15 @@
+import {Component} from "react";
+import "./ClockClass.css";
+
+class ClockClass extends Component {
+
+    public render(): JSX.Element {
+        return (
+            <div className="ClockClass">
+
+            </div>
+        );
+    }
+}
+
+export default ClockClass;
---
+++ src/Components/ex7/ClockFunction/ClockFunction.tsx
@@ -0,0 +1,11 @@
+import "./ClockFunction.css";
+
+function ClockFunction(): JSX.Element {
+    return (
+        <div className="ClockFunction">
+			
+        </div>
+    );
+}
+
+export default ClockFunction;
